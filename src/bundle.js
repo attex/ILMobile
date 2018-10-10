62079f3c479b1b79caf1e32185c804b1014cbe9b
Maximilian Schick <Maximilian.Schick@i-qu.de>
2018-10-10 10:45:59 +0200

    Added windows platform and needed polyfill.

--- src/bundle.js
+++ src/bundle.js
@@ -7,8 +7,12 @@ window.nunjucks = require('nunjucks');
 
 window.JSPath = require('jspath');
 
+//JSON copy method
 if (typeof JSON.clone !== "function") {
     JSON.clone = function(obj) {
         return JSON.parse(JSON.stringify(obj));
     };
 }
+
+//Promise polyfill
+import 'promise-polyfill/src/polyfill';
