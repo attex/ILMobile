b47de831e6d8ddd7bd15e565c8692a73a4169d09
schick <maxi.f.schick@gmail.com>
2019-10-28 12:45:29 +0100

    Add time stamp to output filename

--- www/scripts/clientfunctions/takeAFoto.js
+++ www/scripts/clientfunctions/takeAFoto.js
@@ -1,4 +1,5 @@
 function sendPhoto(path) {
+    var pathWithTimestamp = createFileName(path)
 
     const CAMERA_OPTIONS = {
         // Some common settings are 20, 50, and 100
@@ -14,15 +15,31 @@ function sendPhoto(path) {
         targetHeight: 2000,
     }
 
-    new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         navigator.camera.getPicture(
             base64 => resolve(base64), 
             error => reject("Unable to obtain picture: " + error, "app"),
             CAMERA_OPTIONS);
     }).then(base64 => {
         const fname = 'putDocument'
-        const params = [window.sessionStorage.getItem('ilmSession'), path, base64]
+        const params = [window.sessionStorage.getItem('ilmSession'), pathWithTimestamp, base64]
 
-        handleFunctionOPEN(fname, params, [() => Promise.resolve()])
-    }).catch(outputErrorOPEN)
+        return handleFunctionOPEN(fname, params, [() => Promise.resolve()])
+    }).then(() => sendPhoto(path)).catch(outputErrorOPEN).finally(finish())
+}
+
+function createFileName(path) {
+    var pad = time => `0${time}`.slice(-2)
+
+    var date = new Date();
+    date.setDate(date.getDate());
+    var year = date.getFullYear();
+    var month = pad(date.getMonth() + 1); // "+ 1" becouse the 1st month is 0
+    var day = pad(date.getDate());
+    var hour = pad(date.getHours());
+    var minutes = pad(date.getMinutes());
+    var seconds = pad(date.getSeconds());
+
+    var pathWithoutFileExt = path.slice(0, -5);
+    return `${pathWithoutFileExt}-${year}-${month}-${day}--${hour}-${minutes}-${seconds}.jpeg`
 }
