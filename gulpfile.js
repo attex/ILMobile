9f4946cc5876edb36e735fe686f5e3d860de3fee
schick <maxi.f.schick@gmail.com>
2020-05-28 17:01:39 +0200

    Use manual css

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,7 @@
-// Sass configuration
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var nunjucks = require('gulp-nunjucks');
 var concat = require('gulp-concat');
 var parcel = require('gulp-parcel')
-
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/ilm.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./www/css'));
-});
-
-gulp.task('watch-sass', function(){
-    return gulp.watch('./src/scss/*.scss', gulp.series('sass'))
-});
 
 gulp.task('nunjucks', function () {
     return gulp.src('src/templates/*.njk')
@@ -36,8 +24,8 @@ gulp.task('bundlejs', function () {
 //     return gulp.watch('./www/scripts/tcpConnectionHandler.js', gulp.series('bundlejs'));
 // })
 
-gulp.task('build', gulp.series('sass', 'nunjucks', 'bundlejs'));
-gulp.task('watch', gulp.parallel('watch-sass', 'watch-nunjucks'));
+gulp.task('build', gulp.series('nunjucks', 'bundlejs'));
+gulp.task('watch', gulp.parallel('watch-nunjucks'));
 
 gulp.task('dev', gulp.series('build', 'watch'));
 
